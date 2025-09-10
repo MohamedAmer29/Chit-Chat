efef6863e37e96fb9082e59f8353294f71a29e20
Amer <m1o1h1a1a1@gmail.com>
2025-09-10 03:38:46 +0300

    changed

--- src/components/lib/firebase.ts
+++ src/components/lib/firebase.ts
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 // eslint-disable-next-line
-const app = initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
 export const auth = getAuth();
 export const db = getFirestore();
